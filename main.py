e4014f7e81d7b7327cf19cc38a0d64cfd56c7b05
Sebastian <mail@sebastian-schlecht.de>
2016-06-02 17:08:36 +0200

    Val func

--- main.py
+++ main.py
@@ -1,19 +1,83 @@
 import numpy as np
 import lasagne
-from lasagne.layers import Pool2DLayer
+from threading import Thread
+import Queue
 import theano
 import theano.tensor as T
 import h5py
 import time
 
-DATASET = "./data/nyu_depth_combined_vnet2"
+DATASET_TRAIN = "./data/nyu_depth_combined_vnet2"
+DATASET_VAL = "./data/nyu_depth_combined_vnet2"
 name = "resunet"
-#model = "./data/rs_stack_1_spatial_grad_epoch_250.npz"
-learn_stack = 0
 model = None
+synced_prefetching = True
 
-from networks import vnet, d_rs_stack_1, d_rs_stack_2, residual_unet
-from losses import scale_invariant_error, tukey_biweight, spatial_gradient, mse
+
+q = Queue.Queue(maxsize=20)
+
+from networks import  residual_unet
+from losses import spatial_gradient
+
+
+def prefetch_proc(bs):
+    """
+    Open handle to DB and prefetch data
+    :return:
+    """
+    f = h5py.File(DATASET_TRAIN + ".hdf5")
+    length = f["images"].shape[0]
+    perm = np.random.permutation(length)
+    idx = 0
+    # Subtract mean already and normalize std
+    m = np.load(DATASET_TRAIN + ".npy").astype(np.float32)
+    while True:
+        pp = perm[idx:idx+bs]
+        pp = np.sort(pp).tolist()
+        images = np.array(f["images"][pp])
+        labels = np.array(f["depths"][pp])
+
+        for i in range(len(images)):
+            images[i] = (images[i] - m) / 68.
+        q.put((images, labels), block=True)
+
+        if idx + bs > length:
+            idx = 0
+            perm = np.random.permutation(length)
+        else:
+            idx += bs
+
+
+def start_prefetching_thread(batchsize):
+    thread = Thread(target=prefetch_proc, args=(batchsize,))
+    thread.daemon = True
+    thread.start()
+    return thread
+
+
+def iterate_minibatches_synchronized(inputlen, targetlen, batchsize, shuffle=True, augment=True, downsample=1):
+    """
+    Iterate minibatches from a synced queue
+    :param inputlen:
+    :param targetlen:
+    :param shuffle:
+    :param augment:
+    :param downsample:
+    :return:
+    """
+    assert inputlen == targetlen
+    for start_idx in range(0, inputlen - batchsize + 1, batchsize):
+        # Random crops
+        h = 240
+        w = 320
+        inputs, targets = q.get(block=True)
+        cy = np.random.randint(inputs.shape[2] - h, size=1)
+        cx = np.random.randint(inputs.shape[3] - w, size=1)
+
+        input_cropped = inputs[:, :, cy:cy+h, cx:cx+w].copy()
+        target_cropped = targets[:, cy:cy+h:downsample, cx:cx+w:downsample].copy()
+
+        yield input_cropped, target_cropped
 
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=True, augment=True, downsample=1):
@@ -40,14 +104,14 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=True, augment=True, 
         yield input_cropped, target_cropped
 
 
-def load_data():
-    f = h5py.File(DATASET + ".hdf5")
+def load_val_data():
+    f = h5py.File(DATASET_VAL + ".hdf5")
 
     x_train = np.array(f["images"]).astype(np.float32)
     y_train = np.array(f["depths"]).astype(np.float32)
 
     # Subtract mean already and normalize std
-    m = np.load(DATASET + ".npy").astype(np.float32)
+    m = np.load(DATASET_VAL + ".npy").astype(np.float32)
     for i in range(len(x_train)):
         x_train[i] = (x_train[i] - m) / 68.
 
@@ -63,74 +127,37 @@ def main(num_epochs=200, lr=0.01, batch_size=16):
     # Reshape to enable usage in loss function
     target_reshaped = target_var.dimshuffle((0, "x", 1, 2))
     
-    if learn_stack == 1 or learn_stack == 0:
-        
-        network = residual_unet(input_var=input_var)
-        prediction = lasagne.layers.get_output(network)
+    network = residual_unet(input_var=input_var)
+    prediction = lasagne.layers.get_output(network)
 
-        # Spatial grad / biweight / scale invariant error
-        loss = loss_func(prediction, target_reshaped)
-        # Add some L2
-        all_layers = lasagne.layers.get_all_layers(network)
-        l2_penalty = lasagne.regularization.regularize_layer_params(all_layers, lasagne.regularization.l2) * 0.0001
-        cost = loss + l2_penalty
+    # Spatial grad / biweight / scale invariant error
+    loss = loss_func(prediction, target_reshaped)
+    # Add some L2
+    all_layers = lasagne.layers.get_all_layers(network)
+    l2_penalty = lasagne.regularization.regularize_layer_params(all_layers, lasagne.regularization.l2) * 0.0001
+    cost = loss + l2_penalty
 
-        params = lasagne.layers.get_all_params(network, trainable=True)
-        sh_lr = theano.shared(lasagne.utils.floatX(lr))
-        updates = lasagne.updates.nesterov_momentum(cost, params, learning_rate=sh_lr, momentum=0.9)
-        # Load model weights
-        if model is not None:
-            print "Loading model weights %s" % model
-            with np.load(model) as f:
-                param_values = [f['arr_%d' % i] for i in range(len(f.files))]
-            lasagne.layers.set_all_param_values(network, param_values)
+    params = lasagne.layers.get_all_params(network, trainable=True)
+    sh_lr = theano.shared(lasagne.utils.floatX(lr))
+    updates = lasagne.updates.nesterov_momentum(cost, params, learning_rate=sh_lr, momentum=0.9)
+    # Load model weights
+    if model is not None:
+        print "Loading model weights %s" % model
+        with np.load(model) as f:
+            param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+        lasagne.layers.set_all_param_values(network, param_values)
         
-    
-    elif learn_stack == 2:
-        # Create stack 1
-        in_, stack_1 = d_rs_stack_1(input_var)
-        params_1 = lasagne.layers.get_all_params(stack_1, trainable=True)
-        layers_1 = lasagne.layers.get_all_layers(stack_1)
-        
-        
-        # Create stack 2
-        stack_2 = d_rs_stack_2(input_layer=in_, prev=stack_1)
-        params_2 = lasagne.layers.get_all_params(stack_2, trainable=True)
-        layers_2 = lasagne.layers.get_all_layers(stack_2)
-        
-        layers_top = [l for l in layers_2 if l not in layers_1]
-        
-        # Fix BN gamma and beta for stack 1
-        #_ = lasagne.layers.get_output(stack_1, deterministic=True)
-        # Get output
-        prediction = lasagne.layers.get_output(stack_2)
-        
-        # Compute loss
-        loss = loss_func(prediction, target_reshaped)
-        l2_penalty = lasagne.regularization.regularize_layer_params(layers_top, lasagne.regularization.l2) * 0.0001
-        cost = loss + l2_penalty
-        sh_lr = theano.shared(lasagne.utils.floatX(lr))
-        # Only collect stack 2 weights for updates
-        params = [param for param in params_2 if param not in params_1]
-        
-        updates = lasagne.updates.nesterov_momentum(cost, params, learning_rate=sh_lr, momentum=0.9)
-        network = stack_2
-        # Load model weights
-        if model is not None:
-            with np.load(model) as f:
-                param_values = [f['arr_%d' % i] for i in range(len(f.files))]
-            lasagne.layers.set_all_param_values(layers_1, param_values)
-        
-    
-    
-    
     print "Compiling network"
     # We want to have the main loss only, not l2 values
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
-
-    print "Loading data"
-    X_train, Y_train = load_data()
-
+    val_fn = theano.function([input_var, target_var], [prediction, loss])
+    f = h5py.File(DATASET_TRAIN + ".hdf5")
+    length = f["images"].shape[0]
+    f.close()
+    print "Starting data prefetcher"
+    start_prefetching_thread(batch_size)
+    print "Loading validation data"
+    x_val, y_val = load_val_data()
     print "Starting training"
     train_losses = []
     for epoch in range(num_epochs):
@@ -138,11 +165,6 @@ def main(num_epochs=200, lr=0.01, batch_size=16):
             print "Decreasing LR"
             sh_lr.set_value(lr / 10)
         # shuffle training data
-        train_indices = np.arange(X_train.shape[0])
-        np.random.shuffle(train_indices)
-        X_train = X_train[train_indices, :, :, :]
-        Y_train = Y_train[train_indices, :, :]
-
         # In each epoch, we do a full pass over the training data:
         train_err = 0
         train_batches = 0
@@ -150,13 +172,11 @@ def main(num_epochs=200, lr=0.01, batch_size=16):
         bt = 0
         bidx = 0
         print "Training Epoch %i" % (epoch + 1)
-        
+
+        # Downsample factor
         ds = 1
-        if learn_stack == 1:
-            ds = 4
-        elif learn_stack == 2:
-            ds = 2
-        for batch in iterate_minibatches(X_train, Y_train, batch_size, shuffle=True, augment=True, downsample=ds):
+        # Train for one epoch
+        for batch in iterate_minibatches_synchronized(length, length, batch_size, shuffle=True, augment=True, downsample=ds):
             inputs, targets = batch 
             bts = time.time()
             err = train_fn(inputs, targets)
@@ -166,7 +186,7 @@ def main(num_epochs=200, lr=0.01, batch_size=16):
             if bidx == 20 and epoch == 0:
                 tpb = bt / bidx
                 print "Average time per forward/backward pass: " + str(tpb)
-                eta = time.time() + num_epochs * (tpb * (len(X_train)/batch_size))
+                eta = time.time() + num_epochs * (tpb * (length/batch_size))
                 localtime = time.asctime( time.localtime(eta) )
                 print "ETA: ", localtime
             
@@ -174,10 +194,25 @@ def main(num_epochs=200, lr=0.01, batch_size=16):
             train_err += err
             train_batches += 1
 
+
+        # Validate model
+        v_losses = []
+        v_mse = []
+        for i in range(x_val):
+            x_in = x_val[i,:,9:9+240:ds, 12:12+320:ds]
+            y_in = y_val[i,9:9+240:ds, 12:12+320:ds]
+            v_pred, v_loss = val_fn(x_in, y_in)
+            current_se = (v_pred - y_val[i]) ** 2
+            v_mse.append(current_se)
+            v_losses.append(v_losses)
+        val_mse = np.array(v_mse).mean()
+        val_loss = np.array(v_losses).mean()
         # Then we print the results for this epoch:
         print("Epoch {} of {} took {:.3f}s".format(
             epoch + 1, num_epochs, time.time() - start_time))
         print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+        print("  val loss:\t\t{:.6f}".format(val_loss))
+        print("  val mse:\t\t{:.6f}".format(val_mse))
 
     # Save
     np.savez('./data/' + name +'_epoch_%i.npz' % num_epochs, *lasagne.layers.get_all_param_values(network))
--- test_losses.py
+++ test_losses.py
@@ -1,27 +1,27 @@
 import theano, theano.tensor as T
 import numpy as np
 
-from losses import tukey_biweight, spatial_gradient
+from losses import tukey_biweight, spatial_gradient, _tukey_cost
 
 plot = True
 
-a = np.ones(10000).reshape((10, 10, 10, 10))
-b = np.arange(10000).reshape((10, 10, 10, 10))
+a = np.ones(100000) / 10000.
+b = (np.arange(100000) - 50000.) / 10000.
 
 
 a = a.astype(np.float32)
 b = b.astype(np.float32)
 
-x = T.tensor4("x")
-y = T.tensor4("y")
+x = T.vector("x")
+y = T.vector("y")
 
-z = spatial_gradient(x, y)
+z = _tukey_cost(x - y)
 
 f = theano.function([x, y], z,on_unused_input='warn')
 
 print "MSE is: " + str(((a-b)**2).mean())
 
-r = f(np.log(a),b)
+r = f(a, b)
 print "Custom-Loss is: " + str(r)
 
 if(plot):
